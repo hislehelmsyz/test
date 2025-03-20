7069fc29b170f190a92111c8fe1fdaca6777ee14
hislehelmsyz <hislehelmsyz@outlook.com>
2025-03-20 22:28:49 +0800

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -10,7 +10,6 @@ const screenshotDir = path.join(__dirname, 'screenshot');
 if (!fs.existsSync(screenshotDir)) {
     fs.mkdirSync(screenshotDir);
 }
-
 // Function to launch a browser, capture a screenshot, and fetch QR code
 async function launchBrowserAndCapture(i) {
     console.log(`[${i}] 执行浏览器启动`);
@@ -27,24 +26,29 @@ async function launchBrowserAndCapture(i) {
 
     console.log(`[${i}] 浏览器启动完成`);
 
-    const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
-    await page.setExtraHTTPHeaders({
-        'Accept-Language': 'zh-CN,zh;q=0.9'
-    });
+    let x = 0;
+    
+    while (true) {
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+        await page.setExtraHTTPHeaders({
+            'Accept-Language': 'zh-CN,zh;q=0.9'
+        });
 
-    await page.goto('https://www.qq1.one/user/findpwd.php', {
-        waitUntil: 'networkidle2'
-    });
+        await page.goto('https://www.qq1.one/user/findpwd.php', {
+            waitUntil: 'networkidle2'
+        });
 
-    const qrcodeValue = await page.$eval('#qrimg', (el) => el.getAttribute('qrcode'));
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
-    console.log(`[${i}] 获取成功 ${qrcodeValue}`);
+        const qrcodeValue = await page.$eval('#qrimg', (el) => el.getAttribute('qrcode'));
 
-    // Take a screenshot and save it in the screenshot folder
-    await page.screenshot({ path: path.join(screenshotDir, `screenshot_${i + 1}.png`), fullPage: true });
+        console.log(`[${i}] 获取成功 ${qrcodeValue}`);
 
-    await browser.close();
+        x++;
+        await page.screenshot({ path: path.join(screenshotDir, `screenshot_${i + 1}_${x}.png`), fullPage: true }); 
+        await page.close();
+    }
 }
 
 // Main function to launch multiple browsers concurrently
