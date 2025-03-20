93c006df44e960a8bff164d0c6ad528f531f21ed
hislehelmsyz <hislehelmsyz@outlook.com>
2025-03-20 21:49:23 +0800

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -17,9 +17,15 @@ puppeteer.use(StealthPlugin());
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
 
-    await page.goto('https://www.cloudflare.com/', {
+    await page.setExtraHTTPHeaders({
+        'Accept-Language': 'zh-CN,zh;q=0.9'
+    });
+    
+    await page.goto('https://www.qq1.one/user/findpwd.php', {
         waitUntil: 'networkidle2'
     });
+
+    await page.waitForTimeout(5000);
 
     await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
